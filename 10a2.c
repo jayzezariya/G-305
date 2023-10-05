ff33f0667f9a2f8eda4e27c08439170d6f0aab9d
jayzezariya <145217397+jayzezariya@users.noreply.github.com>
2023-10-05 13:34:43 +0530

    Add files via upload

---
+++ 10a2.c
@@ -0,0 +1,14 @@
+//Find the sum and average of different numbers which are accepted by user as many as user wants
+#include<stdio.h>
+void main(){
+	int i=1,f,l,n,m,a=0;
+	printf("how many numbers do you want average of?  ");
+	scanf("%d",&n);
+    while(i<=n){
+    	printf("input a number: ");
+    	scanf("%d",&m);
+    	a=a+m;i++;
+	}
+	printf("average of all the no.: %d",a/n);
+}       
+                                                                                                                                                                            
